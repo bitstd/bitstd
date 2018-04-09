7fdacf3c0da6aa9e9d7caad35fb223e178a3c881
BitSTD <bitstandard@gmail.com>
2018-04-09 19:20:47 +0800

    achieve BitSTD index

--- src/main/java/com/bitstd/task/BitSTDTask.java
+++ src/main/java/com/bitstd/task/BitSTDTask.java
@@ -252,7 +252,7 @@ public class BitSTDTask {
 		
 		while(true){
 			try{
-				double bp = 0.5;
+				double bp = 1;
 				/*
 				AvgInfoBean btcBean = getBTCIndex();
 				btcBean = getAvgInfoBean(conn,btcBean);
