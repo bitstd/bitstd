e2c31c6ed9069b3fab39e3c129151e427ff43780
joenhon <joenhon@users.noreply.github.com>
2018-06-14 19:31:22 +0800

    joenhon

--- src/contract/airDrop.sol
+++ src/contract/airDrop.sol
@@ -1,4 +1,5 @@
 contract BitSTDView{
+    function symbol()constant  public returns(string) {}
     function migration(address add) public{}
     function transfer(address _to, uint256 _value) public {}
 }
@@ -28,7 +29,7 @@ contract airDrop{
      * @param dsts this is Batch acceptance address
      * @param values This is the distribution number array
      */
-    function airDropValues(address contractaddress,address[] dsts,uint256[] values) public{
+    function airDropValues(address contractaddress,address[] dsts,uint256[] values) public {
 
         uint count= dsts.length;
         BitSTDView View= BitSTDView(contractaddress);
@@ -43,11 +44,26 @@ contract airDrop{
      * @param contractaddress this is Address of airdrop token contract
      * @param dsts This is the address where the data needs to be migrated
      */
-    function dataMigration(address contractaddress,address[] dsts)public {
+    function dataMigration(address contractaddress,address[] dsts)public{
         uint count= dsts.length;
         BitSTDView View= BitSTDView(contractaddress);
         for(uint i = 0; i < count; i++){
            View.migration(dsts[i]);
+        }
+    }
+    /**
+     *
+     *This is Authorization drop
+     * @param _from Assigned address
+     * @param contractaddress this is Address of airdrop token contract
+     * @param dsts this is Batch acceptance address
+     * @param value this is Issuing number
+     */
+    function transferFrom(address contractaddress,address _from, address[] dsts,, uint256 value) public returns (bool success) {
+        uint count= dsts.length;
+        BitSTDView View= BitSTDView(contractaddress);
+        for(uint i = 0; i < count; i++){
+           View.transferFrom(_from,dsts[i],value);
         }
     }
 
